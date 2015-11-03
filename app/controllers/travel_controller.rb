832b94dd3bc3afaf885e859f1118f5211074d287
littlephish <littlephsh@gmail.com>
2015-11-03 00:14:29 -0500

    wip

--- app/controllers/travel_controller.rb
+++ app/controllers/travel_controller.rb
@@ -6,8 +6,12 @@ class TravelController < ApplicationController
   end
 
   def route
-    d = Eve::SolarSystemJump.all.map { |e| [e.from_solar_system_id,e.to_solar_system_id,5] }
+    d = []
+    d.concat JumpBridge.all.map {|j| [j.from_solar_system_id,j.to_solar_system_id,1]}
+    d.concat Eve::SolarSystemJump.all.map { |e| [e.from_solar_system_id,e.to_solar_system_id,5] }
     # TODO: inject bridges
+    logger.debug d.size
+    # logger.debug d
     g = Graph.new(d)
     results = g.shortest_path(params[:travel][:to_system_id].to_i,params[:travel][:from_system_id].to_i)
     logger.debug [params[:travel][:from_system_id].to_i,params[:travel][:to_system_id].to_i]
--- app/models/jump_bridge.rb
+++ app/models/jump_bridge.rb
@@ -1,6 +1,6 @@
 class JumpBridge < ActiveRecord::Base
-  property :from_constellation_id,            type: :integer 
-  property :to_constellation_id,            type: :integer 
+  property :from_solar_system_id,            type: :integer 
+  property :to_solar_system_id,            type: :integer 
   property :from_planet,            type: :integer 
   property :from_moon,            type: :integer 
   property :to_planet,            type: :integer 
