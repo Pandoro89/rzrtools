eb4b38e1c83525f79b1adc89c8b9f1f0dfadd18d
littlephish <littlephsh@gmail.com>
2015-11-04 09:58:46 -0500

    wip

---
+++ app/controllers/admin/watchlists_controller.rb
@@ -0,0 +1,14 @@
+class Admin::WatchlistsController < Admin::ApplicationController
+  before_filter :require_scout_commander_or_higher
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+
+end
