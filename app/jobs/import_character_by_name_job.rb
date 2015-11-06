7d6605c37c26c714971fc4bb5790e9804b6b16b0
littlephish <littlephsh@gmail.com>
2015-11-06 01:13:29 -0500

    wip

--- app/jobs/import_character_by_name_job.rb
+++ app/jobs/import_character_by_name_job.rb
@@ -12,10 +12,14 @@ class ImportCharacterByNameJob < Resque::Job
       result2 = api.CharacterInfo(:characterID => char.id)
       char.corporation_id = result2.corporationID
       char.corp_name = result2.corporation
-      char.alliance_id = result2.allianceID if result2.allianceID
-      char.alliance_name = result2.alliance if result2.alliance
+      begin
+        char.alliance_id = result2.allianceID if result2.allianceID
+        char.alliance_name = result2.alliance if result2.alliance
+        Eve::AllianceCache.add_or_update(result2.allianceID,result2.alliance) if result2.allianceID
+      rescue NoMethodError
+        # NOOP
+      end
       char.save
-      Eve::AllianceCache.add_or_update(result2.allianceID,result2.alliance) if result2.allianceID
       Eve::CorporationCache.add_or_update(result2.corporationID,result2.corporation) if result2.corporationID
       FleetPosition.where(:char_name => name).update_all(:character_id => char.id)
     }
