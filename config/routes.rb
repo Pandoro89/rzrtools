71120421e117f06734aebe867524da336e938920
littlephish <littlephsh@gmail.com>
2016-03-20 12:58:32 -0400

    ip

--- config/routes.rb
+++ config/routes.rb
@@ -85,7 +85,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :characters, only: [:index, :create, :show, :update, :destroy]
-      resources :rash_members, only: [:show, :report]
+      resources :rash_members, only: [:index, :show, :report]
     end
   end
 
