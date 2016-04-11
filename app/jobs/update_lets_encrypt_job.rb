2b179546b2a898b75344afe3accfb9bbbd42a82a
littlephish <littlephsh@gmail.com>
2016-04-11 09:11:05 -0400

    wip:

--- app/jobs/update_lets_encrypt_job.rb
+++ app/jobs/update_lets_encrypt_job.rb
@@ -2,7 +2,8 @@ class UpdateLetsEncryptJob < Resque::Job
   @queue = :high
 
   def self.perform
-    cert_generator = LetsencryptPlugin::CertGenerator.new(LetsencryptPlugin.config.to_h)
+    yml_config = YAML.load_file("#{Rails.root}/config/letsencrypt_plugin.yml") || {}
+    cert_generator = LetsencryptPlugin::CertGenerator.new(yml_config.to_h)
     cert_generator.generate_certificate
   end
 end
